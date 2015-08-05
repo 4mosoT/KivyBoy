d8b0b95edfb0ee0a647f71d778b84c8f34d9d912
Marco <marco.a.formoso@gmail.com>
2015-08-05 14:25:04 +0200

    Sliders 0.3

--- sliders/sliders.py
+++ sliders/sliders.py
@@ -8,15 +8,14 @@ class PipSlider(Slider):
         self.canvas.clear()
         with self.canvas:
 
-            Color(100 / 256., 254 / 256., 181 / 256.)
+            Color(100 / 256., 254 / 256., 181 / 256., 0.3)
             self.rectangle_background = Rectangle(size=(self.width - self.padding, 1), pos=(self.x + self.padding, self.y + self.height/2.))
-
             Color(100 / 256., 254 / 256., 181 / 256.)
-            self.rectangle_cursor= Rectangle(source="piphand2.png",size=(20,30), pos=(self.center_x - 8 , self.value_pos[1] - 3 ))
+            self.rectangle_cursor= Rectangle(source="images/piphand2.png",size=(20,30), pos=(self.center_x - 8 , self.value_pos[1]  ))
 
 
 
     def update(self, *kwargs):
-        self.rectangle_cursor.pos = (self.value_pos[0] - 8, self.value_pos[1] - 3 )
+        self.rectangle_cursor.pos = (self.value_pos[0] - 8, self.value_pos[1] )
 
 
