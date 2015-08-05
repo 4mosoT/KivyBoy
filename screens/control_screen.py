a2cb2965d48a5549e41830e1a50d3a3b8e2e9707
skalextric <marco.a.formoso@gmail.com>
2015-08-05 17:40:02 +0200

    GPIO control for RPi with PIGPIO

--- screens/control_screen.py
+++ screens/control_screen.py
@@ -3,7 +3,7 @@ from sliders.sliders import PipSlider
 from buttons.buttons import PipButton
 from labels.labels import PipLabel
 try:
-    import RPi.GPIO as GPIO
+    import pigpio
 except ImportError:
     pass
 
@@ -12,7 +12,7 @@ class Control (ScreenTmplt):
     def __init__(self, **kwargs):
         super(Control, self).__init__(**kwargs)
         self.add_widget(PipButton(upper=True, text=self.name.upper(), size_hint=(0, 0), size=(50, 20), pos=(25, 215)))
-        self.slide_1 =PipSlider(range=(-5,5), step=0.1, value = 0,size_hint=(0,0), size=(190,20), pos=(70,50))
+        self.slide_1 =PipSlider(range=(-50,50), step=5, value = 0,size_hint=(0,0), size=(190,20), pos=(70,50))
         self.label_1 =PipLabel(size=(50,100),size_hint=(0,0),pos=(20, 10),  text=str(self.slide_1.value))
         self.slide_1.bind(value=self.ValueChange)
         self.add_widget(self.slide_1)
@@ -23,11 +23,7 @@ class Control (ScreenTmplt):
     def ValueChange(self, instance,value):
         self.label_1.text = str(self.slide_1.value)
         try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(18, GPIO.OUT)
-            p=GPIO.PWM(18, 50)
-            p.start(6.9 + self.slide_1.value)
-            p.stop()
-            GPIO.cleanup()
+            p = pigpio.pi()
+            p.set_servo_pulsewidth(18,1500 + self.slide_1.value)
         except:
             pass
