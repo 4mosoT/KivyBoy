b6388033c394892539bda98db26a3d83022a6fae
skalextric <marco.a.formoso@gmail.com>
2015-08-06 18:43:03 +0200

    GPIO control for RPi with PIGPIO

--- screens/control_screen.py
+++ screens/control_screen.py
@@ -12,7 +12,7 @@ class Control (ScreenTmplt):
     def __init__(self, **kwargs):
         super(Control, self).__init__(**kwargs)
         self.add_widget(PipButton(upper=True, text=self.name.upper(), size_hint=(0, 0), size=(50, 20), pos=(25, 215)))
-        self.slide_1 =PipSlider(range=(-50,50), step=5, value = 0,size_hint=(0,0), size=(190,20), pos=(70,50))
+        self.slide_1 =PipSlider(range=(-200,200), step=10, value = 0,size_hint=(0,0), size=(190,20), pos=(70,50))
         self.label_1 =PipLabel(size=(50,100),size_hint=(0,0),pos=(20, 10),  text=str(self.slide_1.value))
         self.slide_1.bind(value=self.ValueChange)
         self.add_widget(self.slide_1)
